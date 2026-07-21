d83e23083d259f42ebba3f6fad61d4adbe7942d3
EhsanArabGhannad <36105412+EhsanArabGhannad@users.noreply.github.com>
2026-07-22 01:59:33 +0300

    Prepare GitHub Pages deployment

--- server.mjs
+++ server.mjs
@@ -18,7 +18,10 @@ const contentTypes = {
   ".json": "application/json; charset=utf-8",
   ".png": "image/png",
   ".svg": "image/svg+xml",
+  ".txt": "text/plain; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".webp": "image/webp",
+  ".xml": "application/xml; charset=utf-8",
 };
 
 const server = createServer(async (request, response) => {
